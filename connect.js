cc824b51cb34dac46be32556b67666e5f3cd493b
Wei-Lin Kolff <94454879+WeiLinKolff@users.noreply.github.com>
2023-03-30 20:29:38 +0200

    asdasdasd

--- connect.js
+++ connect.js
@@ -1,21 +1,34 @@
-var mysql = require('mysql');
+var mysql = require('alaa');
 var express = require('express');
 const app = express();
 const port = 3000;
+const http = require('http');
 
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "alaa"
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
-});
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    fs.readFile('index.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
+}).listen(8080);
