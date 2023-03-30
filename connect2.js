48b775dd0dc267081fe1bf6275cf49a2de3661f0
sjouker01 <sjoukjonker13@gmail.com>
2023-03-31 01:08:46 +0200

    word aan gegroepen

--- connect2.js
+++ connect2.js
@@ -1,25 +1,42 @@
-const mysql = require('mysql2');
-const http = require('http');
-const fs = require('fs');
-const express = require('express');
-const app = express();
-// Maak een verbinding met de database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'alaa'
-});
+// const mysql = require('mysql2');
+// const http = require('http');
+// const fs = require('fs');
+// const express = require('express');
+// const app = express();
+// // Maak een verbinding met de database
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'alaa'
+// });
 
-// connect
-connection.connect((err) => {
-  if(err){
-    throw  err;
-  }
+// // connect
+// connection.connect((err) => {
+//   if(err){
+//     throw  err;
+//   }
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM nodes", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "alaa"
 });
 
 con.connect(function(err) {
   if (err) throw err;
+  //Select all customers and return the result object:
   con.query("SELECT * FROM nodes", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
